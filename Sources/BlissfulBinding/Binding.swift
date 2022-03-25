7ab8b8fd74dfcc9f5034c7103bcd1934974c0bb2
Lawrence Bensaid <lawrencebensaid@icloud.com>
2022-03-25 16:09:00 -0400

    Fixed ?? operator and added event listeners

--- Sources/BlissfulBinding/Binding.swift
+++ Sources/BlissfulBinding/Binding.swift
@@ -6,8 +6,28 @@ extension Binding {
         Binding<Bool> { optionalBinding.wrappedValue != nil } set: { if !$0 { optionalBinding.wrappedValue = nil } }
     }
     
-    public static func ??<T>(lhs: Binding<Optional<T>>, rhs: T) -> Binding<T> {
-        Binding<T> { lhs.wrappedValue ?? rhs } set: { lhs.wrappedValue = $0 }
+    public static func ??(lhs: Binding<Value?>, rhs: Value) -> Binding<Value> {
+        Binding<Value> { lhs.wrappedValue ?? rhs } set: { lhs.wrappedValue = $0 }
+    }
+    
+    public func onChange(_ perform: @escaping (() -> ())) -> Binding<Value> {
+        Binding<Value> { wrappedValue } set: {
+            wrappedValue = $0
+            perform()
+        }
+    }
+    
+}
+
+extension Binding where Value == Bool {
+    
+    public func on(_ value: Bool? = nil, _ perform: @escaping (() -> ())) -> Binding<Value> {
+        Binding<Value> { wrappedValue } set: {
+            wrappedValue = $0
+            if $0 == value {
+                perform()
+            }
+        }
     }
     
 }
