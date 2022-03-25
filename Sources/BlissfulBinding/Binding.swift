45d04c5d522cf620387c9d6bdb89f78763bb7870
Lawrence Bensaid <lawrencebensaid@icloud.com>
2022-03-25 15:35:26 -0400

    Made API accessible

--- Sources/BlissfulBinding/Binding.swift
+++ Sources/BlissfulBinding/Binding.swift
@@ -2,11 +2,11 @@ import SwiftUI
 
 extension Binding {
     
-    static func notNil<T>(_ optionalBinding: Binding<Optional<T>>) -> Binding<Bool> {
+    public static func notNil<T>(_ optionalBinding: Binding<Optional<T>>) -> Binding<Bool> {
         Binding<Bool> { optionalBinding.wrappedValue != nil } set: { if !$0 { optionalBinding.wrappedValue = nil } }
     }
     
-    static func ??<T>(lhs: Binding<Optional<T>>, rhs: T) -> Binding<T> {
+    public static func ??<T>(lhs: Binding<Optional<T>>, rhs: T) -> Binding<T> {
         Binding<T> { lhs.wrappedValue ?? rhs } set: { lhs.wrappedValue = $0 }
     }
     
